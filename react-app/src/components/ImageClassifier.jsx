c1962cae910472d29535ed848f403ee94acf5e5c
michaelfong2017 <michaelfong2017@gmail.com>
2021-03-14 15:26:33 +0800

    Predict images

--- react-app/src/components/ImageClassifier.jsx
+++ react-app/src/components/ImageClassifier.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"; 
+import React, { useEffect } from "react";
 import * as tf from '@tensorflow/tfjs';
 import {
     useRecoilValue,
@@ -6,23 +6,52 @@ import {
 import { imagesAtom } from "components/DropArea"
 
 var model
-const LoadModel = async () => {
+var images
+
+const loadModel = async () => {
     model = await tf.loadLayersModel('https://cnn-repo.s3.ap-east-1.amazonaws.com/models/tfjs/model.json')
     console.log(model)
-    Predict() /* In case the user drag images earlier than model load */
+    return model
 }
 
-const Predict = (images) => {
-    console.log("Predict")
+const loadImageUrl = async (url) => {
+    return new Promise((resolve, reject) => {
+        const im = new Image()
+        im.src = url
+        im.onload = () => {
+            resolve(im)
+        }
+    })
+}
+
+const predict = async (images) => {
+    console.log("predict")
+    console.log(images)
+
+    const predictions = await Promise.all(images.map(async (image) => {
+        const img = await loadImageUrl(image)
+        const tensorImg = tf.browser.fromPixels(img).resizeNearestNeighbor([224, 224]).toFloat().expandDims();
+        const prediction = await model.predict(tensorImg).data();
+        return prediction
+    }))
+
+    return predictions
 }
 
 const ImageClassifier = () => {
-    const images = useRecoilValue(imagesAtom)
+    images = useRecoilValue(imagesAtom)
 
-    Predict(images)
+    if (model) {
+        const predictions = predict(images)
+        console.log(predictions)
+    }
 
     useEffect(() => {
-        LoadModel()
+        loadModel().then(() => {
+            /* In case the user drag images earlier than model load */
+            const predictions = predict(images)
+            console.log(predictions)
+        })
     }, [])
 
     return (
